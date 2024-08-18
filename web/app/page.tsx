e26466fb3ae346ccc32f47abfa1b63529727ece5
SYUJI-SHIKI <syuzi00777@gmail.com>
2024-08-18 13:44:38 +0900

    edit:page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -13,6 +13,8 @@ export default function Home() {
 
       <h1>レイです見えてますか？</h1>
 
+      <h1>見えてます！僕のは見えますか？</h1>
+
       <p>develop</p>
       <div className={styles.grid}>
         {['Feature 1', 'Feature 2', 'Feature 3'].map((feature, index) => (
